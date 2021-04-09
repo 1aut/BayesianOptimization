d5837ff806fdd04471bb27e9da22e34c8028b6b7
1aut <61286629+1aut@users.noreply.github.com>
2021-04-09 15:54:37 +0200

    Update domain_reduction.py
    Removed syntax not compliant to python 2.7

--- bayes_opt/domain_reduction.py
+++ bayes_opt/domain_reduction.py
@@ -26,13 +26,13 @@ class SequentialDomainReductionTransformer(DomainTransformer):
         gamma_osc: float = 0.7,
         gamma_pan: float = 1.0,
         eta: float = 0.9
-    ) -> None:
+    ):
         self.gamma_osc = gamma_osc
         self.gamma_pan = gamma_pan
         self.eta = eta
         pass
 
-    def initialize(self, target_space: TargetSpace) -> None:
+    def initialize(self, target_space: TargetSpace):
         """Initialize all of the parameters"""
         self.original_bounds = np.copy(target_space.bounds)
         self.bounds = [self.original_bounds]
@@ -58,7 +58,7 @@ class SequentialDomainReductionTransformer(DomainTransformer):
 
         self.r = self.contraction_rate * self.r
 
-    def _update(self, target_space: TargetSpace) -> None:
+    def _update(self, target_space: TargetSpace):
 
         # setting the previous
         self.previous_optimal = self.current_optimal
@@ -83,7 +83,7 @@ class SequentialDomainReductionTransformer(DomainTransformer):
 
         self.r = self.contraction_rate * self.r
 
-    def _trim(self, new_bounds: np.array, global_bounds: np.array) -> np.array:
+    def _trim(self, new_bounds: np.array, global_bounds: np.array):
         for i, variable in enumerate(new_bounds):
             if variable[0] < global_bounds[i, 0]:
                 variable[0] = global_bounds[i, 0]
@@ -92,10 +92,10 @@ class SequentialDomainReductionTransformer(DomainTransformer):
 
         return new_bounds
 
-    def _create_bounds(self, parameters: dict, bounds: np.array) -> dict:
+    def _create_bounds(self, parameters: dict, bounds: np.array):
         return {param: bounds[i, :] for i, param in enumerate(parameters)}
 
-    def transform(self, target_space: TargetSpace) -> dict:
+    def transform(self, target_space: TargetSpace):
 
         self._update(target_space)
 
